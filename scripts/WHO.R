d27241741c59c22505948041563c76a2aad17fda
Nick Lauerman <nicholas.lauerman@gmail.com>
2020-03-30 16:22:12 -0500

    update

---
+++ scripts/WHO.R
@@ -0,0 +1,56 @@
+readWHO <- function(path = "./data/WHO/",
+                    file = "report 1.csv",
+                    date = NULL,
+                    reportNumber = 1){
+     rawData <- NULL
+     require(tidyverse)
+     require(lubridate)
+     toRead <- paste(path,
+                     file,
+                     sep = "")
+     rawData <- read_csv(file = toRead)
+     rawData$report <- reportNumber
+     rawData$date <- date
+     rawData$New[is.na(rawData$New)] <- FALSE
+     return(rawData)
+}
+
+WHOClean <- function(dataSet = WHO){
+     dataSet$`Country Territory area`[which(dataSet$`Country Territory area` == "Federal Democratic Republic of Napal")] <-
+          "Napal"
+     dataSet$`Country Territory area`[which(dataSet$`Country Territory area` == "Republic of Singapore")] <-
+          "Singapore"
+
+     return(dataSet)
+}
+
+endReport <- 35
+WHO <- NULL
+startDay <- as.Date("21-Jan-2020", format = "%d-%b-%Y")
+
+for (i in 1:endReport) {
+     reportDate <- startDay + days(i - 1)
+
+     fileName <- paste("report ",
+                       i,
+                       ".csv",
+                       sep = "")
+     temp <- readWHO(file = fileName,
+                     date = reportDate,
+                     reportNumber = i)
+
+     WHO <- rbind(WHO,
+                  temp)
+     rm(temp)
+}
+
+WHO <- WHOClean(dataSet = WHO)
+
+WHO
+table(WHO$`Country Territory area`,
+      useNA = "ifany")
+table(WHO$date,
+      useNA = "ifany")
+table(WHO$report,
+      useNA = "ifany")
+tail(WHO)
