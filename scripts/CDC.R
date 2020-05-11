91dbe5e52e1563673d0e7fdcf042bec66f4b434b
Nick Lauerman <nicholas.lauerman@gmail.com>
2020-05-11 17:20:10 -0500

    New CDC data 11 May

--- scripts/CDC.R
+++ scripts/CDC.R
@@ -2,11 +2,16 @@ require(tidyverse)
 require(lubridate)
 cdc <- read_csv(file = "./data/CDC_data.csv")
 names(cdc)[1:2] <-  c("Date",
-                      "Number of new cases")
-cdc$cum <- cumsum(cdc$`Number of new cases`)
+                      "New Cases")
+cdc$cum <- cumsum(cdc$`New Cases`)
 cdc$Date <- as.Date(cdc$Date,
                     format = "%d-%b-%y")
-cdc$`Week Number` <- week(cdc$Date)
+cdc$`Week Number` <- epiweek(cdc$Date)
+
+cdc$`Total Number of Cases` <- cumsum(cdc$`New Cases`)
+
+
+
 
 cdcClean <- cdc %>%                                     # update with good date
      filter(Date < as.Date("2020-04-03"))
